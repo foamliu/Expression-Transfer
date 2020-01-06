2d38fcf53d2420f0fe4bbfe1b7b1c88ab859237d
Yang Liu <liuyang12@focusmedia.cn>
2020-01-06 14:45:09 +0800

    update

--- extract.py
+++ extract.py
@@ -10,6 +10,36 @@ from config import device
 from utils.ddfa import ToTensorGjz, NormalizeGjz, _parse_param
 from utils.inference import crop_img, parse_roi_box_from_landmark
 
+def extract(filename):
+    img_ori = cv.imread(filename)
+    rects = face_detector(img_ori, 1)
+    rect = rects[0]
+    # bbox = [rect.left(), rect.top(), rect.right(), rect.bottom()]
+    # print('bbox: ' + str(bbox))
+    # roi_box = parse_roi_box_from_bbox(bbox)
+    # print('roi_box: ' + str(roi_box))
+
+    # - use landmark for cropping
+    pts = face_regressor(img_ori, rect).parts()
+    pts = np.array([[pt.x, pt.y] for pt in pts]).T
+    roi_box = parse_roi_box_from_landmark(pts)
+
+    img = crop_img(img_ori, roi_box)
+
+    img = cv.resize(img, (120, 120), interpolation=cv.INTER_LINEAR)
+    input = transform(img).unsqueeze(0)
+    input = input.to(device)
+
+    with torch.no_grad():
+        param = model(input)
+        param = param.squeeze().cpu().numpy().flatten().astype(np.float32)
+
+    # print('param: ' + str(param))
+    p, offset, alpha_shp, alpha_exp = _parse_param(param)
+    # print('alpha_exp: ' + str(alpha_exp))
+    return alpha_exp
+
+
 if __name__ == '__main__':
     filename_scripted = '3ddfa_scripted.pt'
     model = torch.jit.load(filename_scripted)
@@ -28,33 +58,12 @@ if __name__ == '__main__':
 
     for i in tqdm(range(97)):
         filename = 'data/{}.png'.format(i)
-        img_ori = cv.imread(filename)
-        rects = face_detector(img_ori, 1)
-        rect = rects[0]
-        # bbox = [rect.left(), rect.top(), rect.right(), rect.bottom()]
-        # print('bbox: ' + str(bbox))
-        # roi_box = parse_roi_box_from_bbox(bbox)
-        # print('roi_box: ' + str(roi_box))
+        try:
+            alpha_exp = extract(filename)
+            alpha_exp_list.append(alpha_exp)
+        except IndexError as err:
+            print(err)
 
-        # - use landmark for cropping
-        pts = face_regressor(img_ori, rect).parts()
-        pts = np.array([[pt.x, pt.y] for pt in pts]).T
-        roi_box = parse_roi_box_from_landmark(pts)
-
-        img = crop_img(img_ori, roi_box)
-
-        img = cv.resize(img, (120, 120), interpolation=cv.INTER_LINEAR)
-        input = transform(img).unsqueeze(0)
-        input = input.to(device)
-
-        with torch.no_grad():
-            param = model(input)
-            param = param.squeeze().cpu().numpy().flatten().astype(np.float32)
-
-        # print('param: ' + str(param))
-        p, offset, alpha_shp, alpha_exp = _parse_param(param)
-        # print('alpha_exp: ' + str(alpha_exp))
-        alpha_exp_list.append(alpha_exp)
 
     import pickle
 
