bed35a9e5553e70dd0cd90280fb1ee42726f2f06
Yang Liu <liuyang12@focusmedia.cn>
2020-01-06 15:31:59 +0800

    update

--- extract.py
+++ extract.py
@@ -1,5 +1,4 @@
 import cv2 as cv
-import dlib
 import numpy as np
 import torch
 import torch.backends.cudnn as cudnn
@@ -7,24 +6,26 @@ import torchvision.transforms as transforms
 from tqdm import tqdm
 
 from config import device
+from retinaface.detector import Detector
 from utils.ddfa import ToTensorGjz, NormalizeGjz, _parse_param
-from utils.inference import crop_img, parse_roi_box_from_landmark
+from utils.inference import crop_img, parse_roi_box_from_bbox
 
 
 def extract(filename):
     img_ori = cv.imread(filename)
-    img_ori = cv.cvtColor(img_ori, cv.COLOR_BGR2RGB)
-    rects = face_detector(img_ori, 1)
-    rect = rects[0]
-    # bbox = [rect.left(), rect.top(), rect.right(), rect.bottom()]
-    # print('bbox: ' + str(bbox))
-    # roi_box = parse_roi_box_from_bbox(bbox)
-    # print('roi_box: ' + str(roi_box))
+    # rects = face_detector(img_ori, 1)
+    # rect = rects[0]
+    dets, landms = detector.detect_faces(img_ori)
+    det = dets[0]
+    bbox = [det[0], det[1], det[2], det[3]]
+    print('bbox: ' + str(bbox))
+    roi_box = parse_roi_box_from_bbox(bbox)
+    print('roi_box: ' + str(roi_box))
 
     # - use landmark for cropping
-    pts = face_regressor(img_ori, rect).parts()
-    pts = np.array([[pt.x, pt.y] for pt in pts]).T
-    roi_box = parse_roi_box_from_landmark(pts)
+    # pts = face_regressor(img_ori, rect).parts()
+    # pts = np.array([[pt.x, pt.y] for pt in pts]).T
+    # roi_box = parse_roi_box_from_landmark(pts)
 
     img = crop_img(img_ori, roi_box)
 
@@ -50,9 +51,11 @@ if __name__ == '__main__':
     model = model.to(device)
     model.eval()
 
-    face_detector = dlib.get_frontal_face_detector()
-    dlib_landmark_model = 'models/shape_predictor_68_face_landmarks.dat'
-    face_regressor = dlib.shape_predictor(dlib_landmark_model)
+    # face_detector = dlib.get_frontal_face_detector()
+    # dlib_landmark_model = 'models/shape_predictor_68_face_landmarks.dat'
+    # face_regressor = dlib.shape_predictor(dlib_landmark_model)
+
+    detector = Detector()
 
     transform = transforms.Compose([ToTensorGjz(), NormalizeGjz(mean=127.5, std=128)])
 
@@ -60,11 +63,9 @@ if __name__ == '__main__':
 
     for i in tqdm(range(97)):
         filename = 'data/{}.png'.format(i)
-        try:
-            alpha_exp = extract(filename)
-            alpha_exp_list.append(alpha_exp)
-        except IndexError as err:
-            print(err)
+
+        alpha_exp = extract(filename)
+        alpha_exp_list.append(alpha_exp)
 
     import pickle
 
